99ef0382bb0ed2f8c5db7d2d792437138f86e3f0
David Bingham <david.bingham@defra.gov.uk>
2020-01-27 12:30:18 +0000

    Group action rules by type

--- server/services/actionsService.js
+++ server/services/actionsService.js
@@ -7,7 +7,19 @@ async function getActions () {
   const response = await wreck.get(`${config.actionsUrl}/actions`, { json: true })
   console.log('actions retrieved', response.payload)
   const actions = response.payload && response.payload.actions
-  return actions || []
+  return Array.isArray(actions) ? actions.map(parseAction) : []
+}
+
+function parseAction (action) {
+  action.rules = action.rules.map(parseRule)
+  return action
+}
+
+function parseRule (rule) {
+  if (Array.isArray(rule.types) && typeof rule.type === 'undefined') {
+    rule.type = rule.types[0]
+  }
+  return rule
 }
 
 module.exports = {
--- test/services/actionsService.test.js
+++ test/services/actionsService.test.js
@@ -15,21 +15,6 @@ function stubWreckCall () {
   }
 }
 
-const mockActionServiceResult = {
-  actions: [
-    {
-      id: 'ID1',
-      description: 'Test',
-      rules: [{
-        id: 1,
-        description: 'Test rule',
-        enabled: true,
-        facts: []
-      }]
-    }
-  ]
-}
-
 describe('actionService', () => {
   beforeAll(() => {
     // Test follows the structure of parcels-service.test.js from ffc-ce-web, which says:
@@ -43,22 +28,97 @@ describe('actionService', () => {
     actionsService = require('../../server/services/actionsService')
   })
 
-  test('get actions return JSON', async () => {
-    payload = mockActionServiceResult
-    const result = await actionsService.getActions()
-    expect(result).toBeDefined()
-    expect(result.length).toEqual(1)
-    expect(result[0]).toEqual(mockActionServiceResult.actions[0])
-  })
-
-  test('get actions returns empty array for empty payload', async () => {
-    payload = undefined
-    const result = await actionsService.getActions()
-    expect(result).toBeDefined()
-    expect(result.length).toEqual(0)
-  })
-
   afterAll(() => {
     jest.unmock('@hapi/wreck')
+  })
+
+  describe('getActions', () => {
+    test('returns array of actions provided by remote API', async () => {
+      payload = {
+        actions: [
+          {
+            id: 'ID1',
+            description: 'Test',
+            rules: [
+              {
+                id: 1,
+                description: 'Test rule',
+                enabled: true,
+                facts: [],
+                type: 'some-type'
+              }
+            ]
+          }
+        ]
+      }
+      const result = await actionsService.getActions()
+      expect(result).toEqual(payload.actions)
+    })
+
+    test('creates a rule type field using the first item in each rule types array', async () => {
+      payload = {
+        actions: [
+          {
+            id: 'ID1',
+            description: 'Test',
+            rules: [
+              {
+                id: 1,
+                description: 'Test rule 1',
+                enabled: true,
+                facts: [],
+                types: ['some-type', 'some-other-type']
+              },
+              {
+                id: 2,
+                description: 'Test rule 2',
+                enabled: true,
+                facts: [],
+                types: ['some-other-type']
+              }
+            ]
+          }
+        ]
+      }
+      const result = await actionsService.getActions()
+      expect(result).toEqual(expect.any(Array))
+      expect(result.length).toBe(1)
+      expect(result[0].rules.length).toBe(2)
+      expect(result[0].rules[0].type).toBe('some-type')
+      expect(result[0].rules[1].type).toBe('some-other-type')
+    })
+
+    test('does not overwrite an existing rule type field from types array', async () => {
+      payload = {
+        actions: [
+          {
+            id: 'ID1',
+            description: 'Test',
+            rules: [
+              {
+                id: 1,
+                description: 'Test rule',
+                enabled: true,
+                facts: [],
+                type: 'some-type',
+                types: ['some-other-type']
+              }
+            ]
+          }
+        ]
+      }
+      const result = await actionsService.getActions()
+      expect(result).toEqual(expect.any(Array))
+      expect(result.length).toBe(1)
+      expect(result[0].rules.length).toBe(1)
+      expect(result[0].rules[0].type).toBe('some-type')
+    })
+
+    test('returns empty array for empty payload', async () => {
+      payload = undefined
+      const result = await actionsService.getActions()
+      expect(result).toBeDefined()
+      expect(result.length).toBe(0)
+    })
   })
 })
